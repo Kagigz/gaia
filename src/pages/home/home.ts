5be60fb9eb3e11cbcc93fd71f29ff596d50d7b00
Lamine AZINAKOU <agazinakou@gmail.com>
2018-04-23 18:27:20 +0000

    Update home.ts
    Hide splash screen when home page is Ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, Content } from 'ionic-angular';
+import { NavController, Content, Platform } from 'ionic-angular';
 
 import { HomeConvPage } from '../homeConv/homeConv';
 import { HomeInteractPage } from '../homeInteract/homeInteract';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @Component({
   templateUrl: 'home.html'
@@ -12,7 +14,11 @@ export class HomePage {
   tab1Root = HomeInteractPage;
   tab2Root = HomeConvPage;
 
-  constructor() {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      statusBar.styleDefault();
 
+      splashScreen.hide();
+    });
   }
 }
